e3b97332759d45bec57b1ba48310bbc24c242ed8
ryanmccarty <mccarty293@gmail.com>
2019-03-15 19:07:38 -0500

    commiting new files

---
+++ js/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { AppRegistry, TextInput } from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Useless Placeholder'
+    };
+  }
+
+  render() {
+    return (
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(text) => this.setState({ text })}
+        value={this.state.text}
+      />
+    );
+  }
+}

8fcb1eac2c32daa80d09e6a955f7deec0175e8db
ryanmccarty <mccarty293@gmail.com>
2019-03-18 15:12:42 -0500

    changed inital region on mapview, added marker

--- yarg/components/Map.js
+++ yarg/components/Map.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { MapView } from 'expo';
+import { Marker } from 'react-native-maps';
+
 
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 29.9774654,
+        longitude: -90.0758853,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  }
+
   render() {
     return (
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+        initialRegion={this.state.region}
+      >
+      <Marker coordinate={this.state.region}/>
+      </MapView>
     );
   }
 }

1773d658f6cc742c67cedb0303814e797e313941
ryanmccarty <mccarty293@gmail.com>
2019-03-17 19:59:50 -0500

    reinit with expo

---
+++ yarg/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Useless Placeholder'
+    };
+  }
+
+  render() {
+    return (
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(text) => this.setState({ text })}
+        value={this.state.text}
+      />
+    );
+  }
+}

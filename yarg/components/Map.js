5676e74f846bf5ce6ccd2a9fb838fad74e3bab07
Austin Reed <acreed1998@gmail.com>
2019-03-20 13:41:40 -0500

    add basic framework for axios request to get treasures and riddles

--- yarg/components/Map.js
+++ yarg/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapView } from 'expo';
 import { Marker } from 'react-native-maps';
+import Axios from 'axios';
 
 
 export default class Map extends React.Component {
@@ -22,7 +23,21 @@ export default class Map extends React.Component {
     this.setState({ region });
   }
 
+  getTreasuresAndRiddles() {
+    const scope = this;
+    Axios.get(`ec2-3-17-167-48.us-east-2.compute.amazonaws.com/treasures/zipcode?zipcode=${}`).then((treasuresResult) => {
+      Axios.get(`ec2-3-17-167-48.us-east-2.compute.amazonaws.com/riddles/zipcode?zipcode=${}`).then((riddlesResult) => {
+        scope.setState({
+          treasures: treasuresResult.data,
+          riddles: riddlesResult,
+        });
+      }).catch((err) => {
 
+      });
+    }).catch((err) => {
+      
+    });
+  }
 
   render() {
     return (
@@ -32,7 +47,7 @@ export default class Map extends React.Component {
           onRegionChange={this.onRegionChange}
           showsUserLocation={true}
           showsMyLocationButton={true}
-          onUserLocationChange={}
+          onUserLocationChange={this.getTreasuresAndRiddles}
         >
           <Marker coordinate={this.state.region}/>
         </MapView>

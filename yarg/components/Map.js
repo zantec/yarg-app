91eaa37c8f9987b38c3637cecbe049030327432b
ryanmccarty <mccarty293@gmail.com>
2019-03-20 15:26:40 -0500

    bound functions in Map.js

--- yarg/components/Map.js
+++ yarg/components/Map.js
@@ -17,6 +17,8 @@ export default class Map extends React.Component {
       treasures: [],
       riddles: [],
     }
+    this.onRegionChange = this.onRegionChange.bind(this);
+    this.getTreasuresAndRiddles = this.getTreasuresAndRiddles.bind(this);
   }
 
   onRegionChange(region) {

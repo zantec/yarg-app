a4d03687371a514bb17800581731466ae382b716
Austin Reed <acreed1998@gmail.com>
2019-03-23 16:30:58 -0500

    give user some stats

--- yarg/components/profile/Stats.js
+++ yarg/components/profile/Stats.js
@@ -1,14 +1,29 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
 import Constants from 'expo';
 import axios from 'axios';
 
 export default class Stats extends Component {
+  constructor(props) {
+    super(props);
+    props.user = {};
+    props.user.username = 'AND HIS NAME IS JOHN CENA!!!!!!!!';
+    props.user.gold = 100000;
+  }
 
   render() {
     return (
       <View>
-        <Text>Stats</Text>
+        <Text>{this.props.user.username}'s Stats:</Text>
+        <View>
+          <Image source={{ uri: 'https://imgur.com/KfhK2Br.png' }}
+            style={{ width: 50, height: 50, borderRadius: 40 }} />
+          <Text>
+            Username: {this.props.user.username}
+          </Text>
+        </View>
+        <Text>Gold: {this.props.user.gold}</Text>
+
       </View>
     );
   }

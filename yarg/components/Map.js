5f94793b6e04a791146ee12a3316382606d4f578
ryanmccarty <mccarty293@gmail.com>
2019-03-27 20:56:08 -0500

    added styling to riddle/treasure placement overlay on map

--- yarg/components/Map.js
+++ yarg/components/Map.js
@@ -280,5 +280,19 @@ const styles = StyleSheet.create({
     padding: 12,
     minWidth: 56,
     minHeight: 48,
+    color: 'green',
+    width: '40%'
+  },
+  container: {
+    marginLeft: 10,
+    marginRight: 10,
+    width: '100%',
+    alignItems: "stretch",
+    justifyContent: "center"
+  },
+  flex: {
+    flexDirection: 'column',
+    flex: 0,
+    justifyContent: "center"
   },
 });
